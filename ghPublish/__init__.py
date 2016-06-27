36a49f3c9245fbf4bd48641185d3a7c2b5836d52
Mitesh Shah <mitesh@miteshshah.com>
2016-06-27 14:20:05 +0530

    Release version 0.8.2

--- ghPublish/__init__.py
+++ ghPublish/__init__.py
@@ -1,1 +1,2 @@
 from ghPublish.cli import cli
+_VERSION = '0.8.2'
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
+from ghPublish import _VERSION
 setup(
     name='ghPublish',
     packages=['ghPublish'],
-    version='0.8.1',
+    version=_VERSION,
     description='Directly publish your blog posts to GitHub Pages from the command line.',
     long_description='Please visit https://github.com/MiteshNinja/ghPublish for more details.',
     author='Mitesh Shah',
     author_email='mitesh@miteshshah.com',
     url='https://github.com/MiteshNinja/ghPublish',
-    download_url='https://github.com/MiteshNinja/ghPublish/tarball/v0.7',
+    download_url='https://github.com/MiteshNinja/ghPublish/tarball/' +
+    _VERSION,
     keywords=['github', 'publish', 'blog', 'pages', 'cli'],
     classifiers=[],
     install_requires=[
