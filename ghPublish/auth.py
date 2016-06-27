1e8b3e08ffd09879ab88a5d911fe2b31649b7ebf
Mitesh Shah <mitesh@miteshshah.com>
2016-06-27 17:38:19 +0530

    Working authorization module.
    Automatically gets access token if not found.

--- ghPublish/auth.py
+++ ghPublish/auth.py
@@ -1,4 +1,14 @@
 import requests
+import os
+import json
+import getpass
+from requests.auth import HTTPBasicAuth
+
+
+class PassPromptAuth(HTTPBasicAuth):
+    def __init__(self, username):
+        self.username = username
+        self.password = getpass.getpass()
 
 
 class Authorization():
@@ -6,45 +16,67 @@ class Authorization():
     AUTH_URL = 'https://api.github.com/authorizations'
 
     def __init__(self, user):
-        self.user = user
+        self.user = user.lower()
 
-    def get_config(self):
+    def get_auth_details(self):
+        return (self.user, self.get_api_token())
+
+    def get_api_token(self):
+        try:
+            return self._get_config()[self.user]
+        except KeyError:
+            return self._update_config()
+
+    def _get_config(self):
         try:
             if not os.path.exists(self.config_file):
-                open(self.config_file, 'a').close()
-
-            with open(self.config_file) as f:
-                config = json.load(f)
-
+                config = {}
+                with open(self.config_file, 'a') as f:
+                    json.dump(config, f)
+            else:
+                with open(self.config_file) as f:
+                    config = json.load(f)
             return config
-        except IsADirectoryError as err:
-            print(err.args)
+
+        except JSONDecodeError:
+            raise SystemExit(
+                'Your configuration file seems to be broken! JSON cannot be decoded.')
+        except IsADirectoryError:
+            raise SystemExit(
+                "~/.ghPublish must not exist as a directory. Cannot store configuration.")
 
     def _request_access_token(self):
         note = "ghPublish: Directly publish your blog posts to GitHub Pages from the command line."
         note_url = 'https://github.com/MiteshNinja/ghPublish'
         scopes = ["public_repo"]
         payload = dict(note=note, scopes=scopes, note_url=note_url)
+        print("Visit: https://github.com/settings/tokens to manage tokens.\n")
+        print("Requesting one-time ACCESS TOKEN for user: " + self.user)
         r = requests.post(self.AUTH_URL,
-                          auth=(self.user, ),
+                          auth=PassPromptAuth(self.user),
                           data=json.dumps(payload))
 
         rj = r.json()
-        if r.status_code == 201
+        if r.status_code == 201:
             return rj['token']
-        elif r.status_code == 401 and rj['message'].lower() == 'bad credentials':
+        elif r.status_code == 401 and rj['message'].lower(
+        ) == 'bad credentials':
             raise SystemExit('Bad credentials!')
-        elif: r.status_code == 422 and rj['message'].lower() == 'validation failed':
+        elif r.status_code == 422 and rj['message'].lower(
+        ) == 'validation failed':
             raise SystemExit('Validation has failed!')
         else:
             raise SystemExit('An error has occurred while creating a token!')
 
     def _update_config(self):
-        config = self.get_config()
-        token = self.request_access_token()
-        config[user] = token
+        config = self._get_config()
+        token = self._request_access_token()
+        config[self.user] = token
         with open(self.config_file, 'w') as f:
             json.dump(config, f)
+        return token
+
 
 if __name__ == '__main__':
     a = Authorization('MiteshNinja')
+    print(a.get_auth_details())
